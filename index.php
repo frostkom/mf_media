a8d0e06d6a6620c28a1e8400f200e6294a1188c6
Martin Fors <martin.fors@frostkom.se>
2018-09-18 06:18:19 +0200

    Move functions to class

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Media
 Plugin URI: https://github.com/frostkom/mf_media
 Description: 
-Version: 5.6.6
+Version: 5.6.8
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -15,45 +15,44 @@ GitHub Plugin URI: frostkom/mf_media
 */
 
 include_once("include/classes.php");
-include_once("include/functions.php");
 
 $obj_media = new mf_media();
 
 add_action('cron_base', 'activate_media', mt_rand(1, 10));
 
-add_action('init', 'init_media');
-add_action('init', 'init_callback_media', 100);
+add_action('init', array($obj_media, 'init'));
+add_action('init', array($obj_media, 'init_callback'), 100);
 
 if(is_admin())
 {
 	register_activation_hook(__FILE__, 'activate_media');
 	register_uninstall_hook(__FILE__, 'uninstall_media');
 
-	add_action('admin_init', 'settings_media');
+	add_action('admin_init', array($obj_media, 'settings_media'));
 	add_action('admin_init', array($obj_media, 'admin_init'), 0);
 
-	add_action('admin_menu', 'menu_media');
-	add_filter('upload_mimes', 'upload_mimes_media');
+	add_action('admin_menu', array($obj_media, 'admin_menu'));
+	add_filter('upload_mimes', array($obj_media, 'upload_mimes'));
 
 	add_action('wp_handle_upload_prefilter', array($obj_media, 'upload_filter'));
 
-	add_action('rwmb_meta_boxes', 'meta_boxes_media');
+	add_action('rwmb_meta_boxes', array($obj_media, 'rwmb_meta_boxes'));
 
-	add_filter('manage_mf_media_allowed_posts_columns', 'column_header_media_allowed', 5);
-	add_action('manage_mf_media_allowed_posts_custom_column', 'column_cell_media_allowed', 5, 2);
+	add_filter('manage_media_columns', array($obj_media, 'column_header'), 5);
+	add_action('manage_media_custom_column', array($obj_media, 'column_cell'), 5, 2);
+	add_filter('manage_mf_media_allowed_posts_columns', array($obj_media, 'column_header_allowed'), 5);
+	add_action('manage_mf_media_allowed_posts_custom_column', array($obj_media, 'column_cell_allowed'), 5, 2);
 
-	add_filter('filter_on_category', 'filter_on_category_media');
+	add_filter('filter_on_category', array($obj_media, 'filter_on_category'));
 
 	add_action('admin_footer', array($obj_media, 'print_media_templates'), 0);
 
-	add_action('wp_ajax_query-attachments', 'ajax_attachments_media', 0);
-	//add_action('admin_enqueue_scripts', 'enqueue_scripts_media');
+	add_action('wp_ajax_query-attachments', array($obj_media, 'ajax_attachments'), 0);
+	//add_action('admin_enqueue_scripts', array($obj_media, 'admin_enqueue_scripts'));
 
-	add_filter('attachment_fields_to_edit', 'attachment_edit_media', 10, 2);
-	add_action('attachment_fields_to_save', 'attachment_save_media', null, 2);
+	add_filter('attachment_fields_to_edit', array($obj_media, 'attachment_fields_to_edit'), 10, 2);
+	add_action('attachment_fields_to_save', array($obj_media, 'attachment_fields_to_save'), null, 2);
 
-	add_filter('manage_media_columns', 'column_header_media', 5);
-	add_action('manage_media_custom_column', 'column_cell_media', 5, 2);
 
 	load_plugin_textdomain('lang_media', false, dirname(plugin_basename(__FILE__)).'/lang/');
 }
@@ -62,10 +61,10 @@ function activate_media()
 {
 	global $wpdb;
 
+	require_plugin("meta-box/meta-box.php", "Meta Box");
+
 	if(get_option('setting_media_activate_categories') == 'yes')
 	{
-		require_plugin("meta-box/meta-box.php", "Meta Box");
-
 		$default_charset = DB_CHARSET != '' ? DB_CHARSET : "utf8";
 
 		$arr_add_index = array();
@@ -93,36 +92,6 @@ function activate_media()
 		);
 
 		add_index($arr_add_index);
-
-		/* This is not needed anymore since Theme Core handles this */
-		/*$arr_data = array();
-		get_post_children(array('post_type' => 'mf_media_allowed'), $arr_data);
-
-		if(count($arr_data) == 0)
-		{
-			$obj_media = new mf_media();
-
-			$post_data = array(
-				'post_type' => 'mf_media_allowed',
-				'post_status' => 'publish',
-				'post_title' => __("Default", 'lang_media'),
-			);
-
-			$post_id = wp_insert_post($post_data);
-
-			if($post_id > 0)
-			{
-				update_post_meta($post_id, $obj_media->meta_prefix.'action', 'allow');
-
-				$arr_mimes = array('svg', 'eot', 'ttf', 'woff');
-
-				foreach($arr_mimes as $mime)
-				{
-					//update_post_meta() the Meta Box way
-					$wpdb->query($wpdb->prepare("INSERT INTO ".$wpdb->postmeta." SET post_id = '%d', meta_key = %s, meta_value = %s", $post_id, $obj_media->meta_prefix.'types', $mime));
-				}
-			}
-		}*/
 
 		//Migrate from option to DB
 		/*if(IS_ADMIN)
