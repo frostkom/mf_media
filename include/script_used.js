64ab97d6cf19f88bfca9c24bd2a12652857c87d2
frostkom <martin.fors@frostkom.se>
2024-03-13 09:39:57 +0100

    Remove checkbox to do bulk actions on used files

--- include/script_used.js
+++ include/script_used.js
@@ -2,6 +2,9 @@ jQuery(function($)
 {
 	$(".column-used .fa.fa-check.green").each(function()
 	{
-		$(this).parent(".column-used").siblings(".column-title").find(".row-actions .delete").addClass('hide');
+		var dom_obj = $(this).parent(".column-used");
+
+		dom_obj.siblings(".column-title").find(".row-actions .delete").addClass('hide');
+		dom_obj.siblings(".check-column").find("input").remove();
 	});
 });
